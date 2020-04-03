48df086bcc29ce9fd007382e257d580782edfa59
mjnks <sgwood91@gmail.com>
2020-04-03 16:27:16 -0600

    first upload

---
+++ jobs/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template #template folder = ./jobs/templates
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/jobs")
+def jobs():
+    return render_template('index.html')
